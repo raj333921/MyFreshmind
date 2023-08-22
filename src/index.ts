787279ce993ca3cb63984b4d3f8c11a57d3c1d02
vrkgandla <vrkgandla@gmail.com>
2023-08-22 22:51:35 +0200

    Freshly commit with express and postgres connection

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import {User} from "./entity/User";
 
+ const app = express();
+ app.use(bodyParser.json());
+ app.listen(3143)
+
+   Routes.forEach(route => {
+      (app as any)[route.method](route.route, (req:   Request, res: Response, next: Function) => {
+         const result = (new (route.controller as any))[route.action](req, res, next);
+         if (result instanceof Promise) {
+            result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+         } else if (result !== null && result !== undefined) {
+            result.json(result);
+         }
+      });
+   });
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
     console.log("Loading users from the database...")
     const users = await AppDataSource.manager.find(User)
     console.log("Loaded users: ", users)
@@ -18,3 +29,6 @@ AppDataSource.initialize().then(async () => {
     console.log("Here you can setup and run express / fastify / any other framework.")
 
 }).catch(error => console.log(error))
+
+
+
